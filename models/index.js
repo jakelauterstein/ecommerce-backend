8f62f00d536c71bbb872ddcfac4a8564d1d79681
Jake Lauterstein <97852239+jakelauterstein@users.noreply.github.com>
2022-06-01 18:16:38 -0500

    updated routes and models

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 
 Product.belongsTo(Category, {
-  foreignKey: 'category_id',
+  foreignKey: 'category_id'
 });
 
 // Categories have many Products
@@ -20,16 +20,14 @@ Category.hasMany(Product, {
 
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: "unsure",
-  foreignKey: "unsure"
+  foreignKey: "product_id"
 });
 
 // Tags belongToMany Products (through ProductTag)
 
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'unsure',
-  foreignKey: 'unsure'
+  foreignKey: 'tag_id'
 });
 
 module.exports = {
